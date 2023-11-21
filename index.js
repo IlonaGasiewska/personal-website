cc3b14c5c4302052f96f62eee216a8055f3136ce
ilona <ilona.gasiewska.ilona@gmail.com>
2023-11-21 22:33:16 +0100

    1th commit - navbar

---
+++ index.js
@@ -0,0 +1,26 @@
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", ()=>{window.pageYOffset === 0 ? navbar.classList.remove("active-navbar") : navbar.classList.add("active-navbar")});
+
+
+///////////////////////////////////////////////////////////////
+
+const themeSwitcher = document.querySelector(".theme-switcher");
+
+const body = document.querySelector("body");
+const container = document.querySelector(".container")
+
+themeSwitcher.addEventListener("click",()=>{body.classList.toggle("light-theme");
+navbar.classList.toggle("navbar-light-theme");
+container.classList.toggle("light-theme");
+themeSwitcher.classList.toggle("theme-switcher-light-theme");
+});
+
+
+///////////////////////////////////////////////////////////////
+
+const menuIcon = document.querySelector(".menu-icon");
+const menuList = document.querySelector(".navbar-list");
+
+menuIcon.addEventListener("click",()=>{menuList.classList.toggle("navbar-list-active")})
+
