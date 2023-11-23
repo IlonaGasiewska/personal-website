1095bad2055b8fdf98bc4f55c728fe1ba5041287
Ilona Gąsiewska <i.gasiewska.ilona@gmail.com>
2023-11-23 22:01:12 +0100

    start section

--- index.js
+++ index.js
@@ -22,5 +22,12 @@ themeSwitcher.classList.toggle("theme-switcher-light-theme");
 const menuIcon = document.querySelector(".menu-icon");
 const menuList = document.querySelector(".navbar-list");
 
-menuIcon.addEventListener("click",()=>{menuList.classList.toggle("navbar-list-active")})
+menuIcon.addEventListener("click",()=>{menuList.classList.toggle("navbar-list-active");
+navbar.classList.toggle("active-list");
+})
 
+window.addEventListener("resize",()=>{  
+    if(window.innerWidth > 1000){
+    navbar.classList.toggle("active-list")
+}})
+console.log(window.innerWidth)
